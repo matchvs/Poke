4901cf940cd54f8b94caaf324ff32f436d89fc54
songwei <songwei@matchvs.com>
2018-06-13 17:08:27 +0800

    替换base64 加密算法

--- framework/Basic/src/utils/ArrayTools.ts
+++ framework/Basic/src/utils/ArrayTools.ts
@@ -23,100 +23,180 @@ class ArrayTools {
         });
     }
 
-    public static Base64Encode(input){
-        var output = "";
-        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-        var i = 0;
-        input = ArrayTools._utf8_encode(input);
-        while (i < input.length) {
-            chr1 = input.charCodeAt(i++);
-            chr2 = input.charCodeAt(i++);
-            chr3 = input.charCodeAt(i++);
-            enc1 = chr1 >> 2;
-            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-            enc4 = chr3 & 63;
-            if (isNaN(chr2)) {
-                enc3 = enc4 = 64;
-            } else if (isNaN(chr3)) {
-                enc4 = 64;
-            }
-            output = output +
-                ArrayTools._b64keyStr.charAt(enc1) + ArrayTools._b64keyStr.charAt(enc2) +
-                ArrayTools._b64keyStr.charAt(enc3) + ArrayTools._b64keyStr.charAt(enc4);
-        }
-        return output;
-    }
+    /**
+    * UTF16和UTF8转换对照表
+    * U+00000000 – U+0000007F 	0xxxxxxx
+    * U+00000080 – U+000007FF 	110xxxxx 10xxxxxx
+    * U+00000800 – U+0000FFFF 	1110xxxx 10xxxxxx 10xxxxxx
+    * U+00010000 – U+001FFFFF 	11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+    * U+00200000 – U+03FFFFFF 	111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+    * U+04000000 – U+7FFFFFFF 	1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+    */
+	// 转码表
+	private static table = [
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+			'I', 'J', 'K', 'L', 'M', 'N', 'O' ,'P',
+			'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+			'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+			'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+			'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+			'w', 'x', 'y', 'z', '0', '1', '2', '3',
+			'4', '5', '6', '7', '8', '9', '+', '/'
+	];
 
-    public static Base64Decode(input){
-        var output = "";
-        var chr1, chr2, chr3;
-        var enc1, enc2, enc3, enc4;
-        var i = 0;
-        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-        while (i < input.length) {
-            enc1 = ArrayTools._b64keyStr.indexOf(input.charAt(i++));
-            enc2 = ArrayTools._b64keyStr.indexOf(input.charAt(i++));
-            enc3 = ArrayTools._b64keyStr.indexOf(input.charAt(i++));
-            enc4 = ArrayTools._b64keyStr.indexOf(input.charAt(i++));
-            chr1 = (enc1 << 2) | (enc2 >> 4);
-            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-            chr3 = ((enc3 & 3) << 6) | enc4;
-            output = output + String.fromCharCode(chr1);
-            if (enc3 != 64) {
-                output = output + String.fromCharCode(chr2);
-            }
-            if (enc4 != 64) {
-                output = output + String.fromCharCode(chr3);
-            }
-        }
-        output = ArrayTools._utf8_decode(output);
-        return output;
-    }
+	public static UTF16ToUTF8(str) {
+		var res = [], len = str.length;
+		for (var i = 0; i < len; i++) {
+			var code = str.charCodeAt(i);
+			if (code > 0x0000 && code <= 0x007F) {
+				// 单字节，这里并不考虑0x0000，因为它是空字节
+				// U+00000000 – U+0000007F 	0xxxxxxx
+				res.push(str.charAt(i));
+			} else if (code >= 0x0080 && code <= 0x07FF) {
+				// 双字节
+				// U+00000080 – U+000007FF 	110xxxxx 10xxxxxx
+				// 110xxxxx
+				var byte1 = 0xC0 | ((code >> 6) & 0x1F);
+				// 10xxxxxx
+				var byte2 = 0x80 | (code & 0x3F);
+				res.push(
+					String.fromCharCode(byte1), 
+					String.fromCharCode(byte2)
+				);
+			} else if (code >= 0x0800 && code <= 0xFFFF) {
+				// 三字节
+				// U+00000800 – U+0000FFFF 	1110xxxx 10xxxxxx 10xxxxxx
+				// 1110xxxx
+				var byte1 = 0xE0 | ((code >> 12) & 0x0F);
+				// 10xxxxxx
+				var byte2 = 0x80 | ((code >> 6) & 0x3F);
+				// 10xxxxxx
+				var byte3 = 0x80 | (code & 0x3F);
+				res.push(
+					String.fromCharCode(byte1), 
+					String.fromCharCode(byte2), 
+					String.fromCharCode(byte3)
+				);
+			} else if (code >= 0x00010000 && code <= 0x001FFFFF) {
+				// 四字节
+				// U+00010000 – U+001FFFFF 	11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+			} else if (code >= 0x00200000 && code <= 0x03FFFFFF) {
+				// 五字节
+				// U+00200000 – U+03FFFFFF 	111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+			} else /** if (code >= 0x04000000 && code <= 0x7FFFFFFF)*/ {
+				// 六字节
+				// U+04000000 – U+7FFFFFFF 	1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+			}
+		}
+		return res.join('');
+	}
 
+	public static UTF8ToUTF16(str) {
+		var res = [], len = str.length;
+		var i = 0;
+		for (var i = 0; i < len; i++) {
+			var code = str.charCodeAt(i);
+			// 对第一个字节进行判断
+			if (((code >> 7) & 0xFF) == 0x0) {
+				// 单字节
+				// 0xxxxxxx
+				res.push(str.charAt(i));
+			} else if (((code >> 5) & 0xFF) == 0x6) {
+				// 双字节
+				// 110xxxxx 10xxxxxx
+				var code2 = str.charCodeAt(++i);
+				var byte1 = (code & 0x1F) << 6;
+				var byte2 = code2 & 0x3F;
+				var utf16 = byte1 | byte2;
+				res.push(String.fromCharCode(utf16));
+			} else if (((code >> 4) & 0xFF) == 0xE) {
+				// 三字节
+				// 1110xxxx 10xxxxxx 10xxxxxx
+				var code2 = str.charCodeAt(++i);
+				var code3 = str.charCodeAt(++i);
+				var byte1 = (code << 4) | ((code2 >> 2) & 0x0F);
+				var byte2 = ((code2 & 0x03) << 6) | (code3 & 0x3F);
+				var utf16 = ((byte1 & 0x00FF) << 8) | byte2
+				res.push(String.fromCharCode(utf16));
+			} else if (((code >> 3) & 0xFF) == 0x1E) {
+				// 四字节
+				// 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+			} else if (((code >> 2) & 0xFF) == 0x3E) {
+				// 五字节
+				// 111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+			} else /** if (((code >> 1) & 0xFF) == 0x7E)*/ {
+				// 六字节
+				// 1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+			}
+		}
 
-    // private method for UTF-8 encoding
-    private static _utf8_encode = function (string) {
-        string = string.replace(/\r\n/g,"\n");
-        var utftext = "";
-        for (var n = 0; n < string.length; n++) {
-            var c = string.charCodeAt(n);
-            if (c < 128) {
-                utftext += String.fromCharCode(c);
-            } else if((c > 127) && (c < 2048)) {
-                utftext += String.fromCharCode((c >> 6) | 192);
-                utftext += String.fromCharCode((c & 63) | 128);
-            } else {
-                utftext += String.fromCharCode((c >> 12) | 224);
-                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-                utftext += String.fromCharCode((c & 63) | 128);
-            }
+		return res.join('');
+	}
 
-        }
-        return utftext;
-    };
+	public static Base64Encode(str) {
+		if (!str) {
+			return '';
+		}
+		var utf8    = this.UTF16ToUTF8(str); // 转成UTF8
+		var i = 0; // 遍历索引
+		var len = utf8.length;
+		var res = [];
+		while (i < len) {
+			var c1 = utf8.charCodeAt(i++) & 0xFF;
+			res.push(this.table[c1 >> 2]);
+			// 需要补2个=
+			if (i == len) {
+				res.push(this.table[(c1 & 0x3) << 4]);
+				res.push('==');
+				break;
+			}
+			var c2 = utf8.charCodeAt(i++);
+			// 需要补1个=
+			if (i == len) {
+				res.push(this.table[((c1 & 0x3) << 4) | ((c2 >> 4) & 0x0F)]);
+				res.push(this.table[(c2 & 0x0F) << 2]);
+				res.push('=');
+				break;
+			}
+			var c3 = utf8.charCodeAt(i++);
+			res.push(this.table[((c1 & 0x3) << 4) | ((c2 >> 4) & 0x0F)]);
+			res.push(this.table[((c2 & 0x0F) << 2) | ((c3 & 0xC0) >> 6)]);
+			res.push(this.table[c3 & 0x3F]);
+		}
 
-    // private method for UTF-8 decoding
-    private static _utf8_decode = function (utftext) {
-        var string = "";
-        var i = 0;
-        let c =0,c1=0 , c2 = 0;
-        while ( i < utftext.length ) {
-            c = utftext.charCodeAt(i);
-            if (c < 128) {
-                string += String.fromCharCode(c);
-                i++;
-            } else if((c > 191) && (c < 224)) {
-                c2 = utftext.charCodeAt(i+1);
-                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-                i += 2;
-            } else {
-                c2 = utftext.charCodeAt(i+1);
-                let c3 = utftext.charCodeAt(i+2);
-                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-                i += 3;
-            }
-        }
-        return string;
-    };
+		return res.join('');
+	}
+
+	public static Base64Decode(str) {
+		if (!str) {
+			return '';
+		}
+		var len = str.length;
+		var i   = 0;
+		var res = [];
+        var code1 = 0;
+        var code2 = 0;
+        var code3 = 0;
+        var code4 = 0;
+		while (i < len) {
+			code1 = this.table.indexOf(str.charAt(i++));
+			code2 = this.table.indexOf(str.charAt(i++));
+			code3 = this.table.indexOf(str.charAt(i++));
+			code4 = this.table.indexOf(str.charAt(i++));
+
+			let c1 = (code1 << 2) | (code2 >> 4);
+			res.push(String.fromCharCode(c1));
+
+			if (code3 != -1) {
+				let c2 = ((code2 & 0xF) << 4) | (code3 >> 2);
+				res.push(String.fromCharCode(c2));
+			}
+			if (code4 != -1) {
+				let c3 = ((code3 & 0x3) << 6) | code4;
+				res.push(String.fromCharCode(c3));
+			}
+		}
+		return this.UTF8ToUTF16(res.join(''));
+	}
+
 }
